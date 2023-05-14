bb9e71f9e871f36e51630f15eb67ffc965590109
north-tower <muneshmyke@gmail.com>
2023-05-14 22:54:39 +0300

    'app'

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-react-native-classnames"
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ResturantScreen from './screens/ResturantScreen';
+import 'react-native-gesture-handler';
+
+
+
+
+
+export default function App() {
+const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}  options={{
+                headerShown: false,
+              }}/>
+          <Stack.Screen name="Resturant" component={ResturantScreen}  options={{
+                headerShown: false,
+              }}/>
+        </Stack.Navigator>
+      </SafeAreaProvider>
+      
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
